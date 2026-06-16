fa0ba4c4ffa214aa57c7c13bfbb7e4c33c3dec33
Jay Lee <jabang3@gmail.com>
2026-06-15 17:17:06 -0700

    Adapt Astro 6 dependency update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jayleekr.github.io',
 
 	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
 		mdx(),
 		sitemap({
 			changefreq: 'weekly',
